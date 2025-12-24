653ad7a633d5076e95b548844d37598e87aad502
ImLunchtime <knoxmedia@yeah.net>
2025-12-24 16:22:41 +0800

    新物体

--- Placeables/Scripts/PlaceableRegistry.gd
+++ Placeables/Scripts/PlaceableRegistry.gd
@@ -5,7 +5,8 @@ signal changed
 
 
 func _enter_tree():
-	register_item("k_claw_machine", "res://Placeables/KClawMachine.tscn", "KClaw Machine", "", "Props")
+	register_item("k_claw_machine", "res://Placeables/KClawMachine.tscn", "[Kenney] Claw Machine", "", "Props")
+	register_item("k_cash_register", "res://Placeables/KCashRegister.tscn", "[Kenney] Cash Register", "", "Props")
 
 func register_item(id: String, scene_path: String, display_name: String, icon_path: String = "", category: String = "Props"):
 	items[id] = {
